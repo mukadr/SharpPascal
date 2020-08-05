a9b69ec3be019f0907c56d4838ea100ee8632955
Mauro Dreissig <mukadr@gmail.com>
2020-08-05 15:05:40 -0300

    Add SharpPascal.Tests project

---
+++ src/SharpPascal.Tests/Syntax/AbstractSyntaxTreeTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpPascal.Syntax;
+
+namespace SharpPascal.Tests
+{
+    [TestClass]
+    public class AbstractSyntaxTreeTests
+    {
+        [TestMethod]
+        public void AbstractSyntaxTree_Equals_Test()
+        {
+            var expr1 = new AddExpression(new IntegerExpression(100), new IntegerExpression(200));
+            var expr2 = new SubExpression(new IntegerExpression(100), new IntegerExpression(200));
+            var expr3 = new AddExpression(new IntegerExpression(100), new IntegerExpression(300));
+            var expr4 = new AddExpression(new IntegerExpression(100), new IntegerExpression(200));
+
+            Assert.AreNotEqual(expr1, expr2);
+            Assert.AreNotEqual(expr1, expr3);
+            Assert.AreNotEqual(expr2, expr3);
+            Assert.AreEqual(expr1, expr4);
+        }
+    }
+}
