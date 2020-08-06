b8461d5bd7f48ba4b219980d4c85a903843d5723
Mauro Dreissig <mukadr@gmail.com>
2020-08-06 13:51:36 -0300

    Introduce PascalParser

---
+++ src/SharpPascal.Tests/Syntax/PascalParserTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpPascal.Syntax;
+using SharpPascal.Syntax.Parsing;
+
+namespace SharpPascal.Tests.Syntax
+{
+    [TestClass]
+    public class PascalParserTests
+    {
+        [TestMethod]
+        public void SkipWhiteTest()
+        {
+            var source = new Source("\t\t\n\n\r\r     \r\r\n\r    \t \t \n");
+
+            var (tree, line) = PascalParser.Parse(source);
+
+            Assert.IsNull(tree);
+            Assert.AreEqual(5, line);
+
+            source = new Source(@"
+                {
+                    Hello World!
+                }");
+
+            (tree, line) = PascalParser.Parse(source);
+
+            Assert.IsNull(tree);
+            Assert.AreEqual(4, line);
+        }
+
+        [TestMethod]
+        public void UnterminatedCommentTest()
+        {
+            var source = new Source(@"
+                {
+                    Hello World!
+                ");
+
+            Assert.ThrowsException<ParseException>(() => PascalParser.Parse(source));
+        }
+
+        [TestMethod]
+        public void ParseNumberTest()
+        {
+            var source = new Source(@"
+                150
+            ");
+
+            var tree = PascalParser.Parse(source).Tree;
+
+            Assert.IsNotNull(tree);
+            Assert.AreEqual(2, tree?.Location.Line);
+            Assert.IsInstanceOfType(tree, typeof(IntegerExpression));
+            Assert.AreEqual(150, ((IntegerExpression?)tree)?.Value);
+        }
+    }
+}
---
+++ src/SharpPascal/Syntax/PascalParser.cs
@@ -0,0 +1,53 @@
+﻿using SharpPascal.Syntax.Parsing;
+using System;
+using System.Linq;
+using static SharpPascal.Syntax.Parsing.ParserFactory;
+
+namespace SharpPascal.Syntax
+{
+    public static class PascalParser
+    {
+        public static (AbstractSyntaxTree? Tree, int Line) Parse(Source source)
+        {
+            var currentLine = 1;
+
+            Func<string, string> report = (error) => string.Format("%d: %s", currentLine, error);
+
+            var whitespace =
+                OneOrMore(
+                    Symbol(' ')
+                    .Or(Symbol('\t'))
+                    .Or(Symbol('\n').Bind(c =>
+                    {
+                        currentLine++;
+                        return Constant(c);
+                    }))
+                    .Or(Symbol('\r'))
+                );
+
+            var multilineComment =
+                Symbol('{')
+                .And(Until('}').OrError(report("Expected '}' before end of source")))
+                .Bind(text =>
+                {
+                    currentLine += text.Count(c => c == '\n');
+                    return Constant(text);
+                });
+
+            var skipWhite =
+                OneOrMore(whitespace.Or(multilineComment));
+
+            var digit =
+                Symbol('0', '9');
+
+            var integer =
+                OneOrMore(digit)
+                .Map(value => new IntegerExpression(int.Parse(value), new Location(currentLine)));
+
+            var program =
+                skipWhite.Anyway(integer);
+
+            return (program.Parse(source)?.Value, currentLine);
+        }
+    }
+}
