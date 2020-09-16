c4f2b694e8870b555fe164fda718535f858919ec
Mauro Dreissig <mukadr@gmail.com>
2020-09-16 12:50:50 -0300

    Introduce Binding
    A bound tree associates semantic information to a syntax tree.

---
+++ src/SharpPascal.Tests/Binding/BindingTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpPascal.Binding;
+using SharpPascal.Syntax;
+
+namespace SharpPascal.Tests.Syntax
+{
+    [TestClass]
+    public class BindingTests
+    {
+        [TestMethod]
+        public void Binder_Declares_Variables()
+        {
+            var source = @"
+                var
+                    x: integer;
+                    y: integer;
+                    z: integer;
+
+                begin end.
+            ";
+
+            var tree = PascalParser.Parse(source);
+
+            Assert.IsNotNull(tree);
+
+            var boundUnit = Binder.SyntaxCheck(tree!);
+
+            var x = boundUnit.Scope.Find("x");
+            var y = boundUnit.Scope.Find("y");
+            var z = boundUnit.Scope.Find("z");
+
+            Assert.IsNotNull(x);
+            Assert.IsNotNull(y);
+            Assert.IsNotNull(z);
+
+            Assert.AreEqual("integer", x.Type.Name);
+            Assert.AreEqual("integer", y.Type.Name);
+            Assert.AreEqual("integer", z.Type.Name);
+        }
+    }
+}
