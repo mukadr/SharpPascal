47f5b01e686a3ae564bf040756f64f7db74ad09a
Mauro Dreissig <mukadr@gmail.com>
2020-11-10 12:26:11 -0300

    PascalParser: Better indentation

--- src/SharpPascal/Syntax/PascalParser.cs
+++ src/SharpPascal/Syntax/PascalParser.cs
@@ -13,17 +13,14 @@ namespace SharpPascal.Syntax
                 Regex("[ \t\n\r]+");
 
             var multilineComment =
-                Regex("{")
-                .And(Regex("[^}]*}")
-                .Or(new Parser<string>(_ => throw new ParseException("expected '}' before end of source"))));
+                Regex("{").And(Regex("[^}]*}").Or(new Parser<string>(_ =>
+                    throw new ParseException("expected '}' before end of source"))));
 
             var blank =
                 OneOrMore(whitespace.Or(multilineComment));
 
             Parser<(string text, Location location)> Op(string regex) =>
-                Regex(regex)
-                .Map((text, location) => (text, location))
-                .Skip(blank);
+                Regex(regex).Map((text, location) => (text, location)).Skip(blank);
 
             var assign = Op(":=");
             var add = Op("\\+");
@@ -43,9 +40,7 @@ namespace SharpPascal.Syntax
             var dot = Op("\\.");
 
             Parser<(string text, Location location)> Kw(string name) =>
-                Regex(name + "\\b", true)
-                .Map((text, location) => (text, location))
-                .Skip(blank);
+                Regex(name + "\\b", true).Map((text, location) => (text, location)).Skip(blank);
 
             var begin = Kw("begin");
             var div = Kw("div");
@@ -71,39 +66,41 @@ namespace SharpPascal.Syntax
 
             var id =
                 SNot(keyword)
-                .And(Regex("[a-zA-Z_][a-zA-Z_0-9]*"))
-                .Map((text, location) => (text, location))
+                .And(Regex("[a-zA-Z_][a-zA-Z_0-9]*")).Map((text, location) => (text, location))
                 .Skip(blank);
 
             var integer =
-                Regex("[0-9]+")
-                .Map<ExpressionSyntax>((value, location) => new IntegerExpressionSyntax(int.Parse(value), location))
+                Regex("[0-9]+").Map<ExpressionSyntax>((value, location) =>
+                    new IntegerExpressionSyntax(int.Parse(value), location))
                 .Skip(blank);
 
             var @string =
-                Regex("'[^('\n\r)]*'")
-                .Map<ExpressionSyntax>((value, location) => new StringExpressionSyntax(value.Substring(1, value.Length - 2), location))
+                Regex("'[^('\n\r)]*'").Map<ExpressionSyntax>((value, location) =>
+                    new StringExpressionSyntax(value.Substring(1, value.Length - 2), location))
                 .Skip(blank);
 
             var variable =
-                id.Map<ExpressionSyntax>(idToken => new VarExpressionSyntax(idToken.text, idToken.location));
+                id.Map<ExpressionSyntax>(idToken =>
+                    new VarExpressionSyntax(idToken.text, idToken.location));
 
             var expression =
                 Forward<ExpressionSyntax>();
 
             var arguments =
                 expression.Bind(arg =>
-                    ZeroOrMore(comma.And(expression)).Map(args =>
-                    {
-                        args.Insert(0, arg);
-                        return args;
-                    }))
+                    ZeroOrMore(
+                        comma.And(expression)).Map(args =>
+                        {
+                            args.Insert(0, arg);
+                            return args;
+                        }))
                 .Or(Constant(new List<ExpressionSyntax>()));
 
             var call =
                 id.Bind(idToken =>
-                    lparen.And(arguments.Bind(args =>
-                        rparen.Map<ExpressionSyntax>(_ => new CallExpressionSyntax(idToken.text, args, idToken.location)))));
+                    lparen.And(arguments).Bind(args =>
+                        rparen.Map<ExpressionSyntax>(_ =>
+                            new CallExpressionSyntax(idToken.text, args, idToken.location))));
 
             var factor =
                 integer
@@ -112,25 +109,33 @@ namespace SharpPascal.Syntax
                 .Or(variable)
                 .Or(lparen.And(expression).Bind(expr => rparen.And(Constant(expr))));
 
-            Parser<ExpressionSyntax> binaryOperator(Parser<ExpressionSyntax> expr, Parser<(string text, Location location)> op) =>
+            Parser<ExpressionSyntax> BinaryOperator(
+                Parser<ExpressionSyntax> expr,
+                Parser<(string text, Location location)> op)
+            =>
                 expr.Bind(first =>
-                    ZeroOrMore(op.Bind(opToken =>
-                        expr.Map(right =>
-                            (opToken, right)))).Map(operatorTerms =>
-                                operatorTerms.Aggregate(first, (left, ot) =>
-                                    BinaryExpressionSyntax.CreateInstance(left, ot.opToken.text, ot.right, ot.opToken.location))));
+                    ZeroOrMore(
+                        op.Bind(opToken =>
+                            expr.Map(right => (opToken, right))))
+                    .Map(operatorTerms =>
+                        operatorTerms.Aggregate(first, (left, ot) =>
+                            BinaryExpressionSyntax.CreateInstance(
+                                left,
+                                ot.opToken.text,
+                                ot.right,
+                                ot.opToken.location))));
 
             var mulExpression =
-                binaryOperator(factor, mul.Or(div).Or(mod));
+                BinaryOperator(factor, mul.Or(div).Or(mod));
 
             var addExpression =
-                binaryOperator(mulExpression, add.Or(sub));
+                BinaryOperator(mulExpression, add.Or(sub));
 
             var ltExpression =
-                binaryOperator(addExpression, le.Or(ge).Or(lt).Or(gt));
+                BinaryOperator(addExpression, le.Or(ge).Or(lt).Or(gt));
 
             var eqExpression =
-                binaryOperator(ltExpression, eq.Or(ne));
+                BinaryOperator(ltExpression, eq.Or(ne));
 
             expression.Parse =
                 eqExpression.Parse;
@@ -141,32 +146,35 @@ namespace SharpPascal.Syntax
             var ifStatement =
                 @if.Bind(ifToken =>
                     expression.Bind(expr =>
-                        then.And(statement.Bind(trueStmt =>
+                        then.And(statement).Bind(trueStmt =>
                             Maybe(@else.And(statement)).Map<StatementSyntax>(falseStmt =>
-                                new IfStatementSyntax(expr, trueStmt, falseStmt, ifToken.location))))));
+                                new IfStatementSyntax(expr, trueStmt, falseStmt, ifToken.location)))));
 
             var whileStatement =
                 @while.Bind(whileToken =>
                     expression.Bind(expr =>
-                        @do.And(statement.Map<StatementSyntax>(stmt =>
-                            new WhileStatementSyntax(expr, stmt, whileToken.location)))));
+                        @do.And(statement).Map<StatementSyntax>(stmt =>
+                            new WhileStatementSyntax(expr, stmt, whileToken.location))));
 
             var assignmentStatement =
                 id.Bind(idToken =>
-                    assign.And(expression.Map<StatementSyntax>(expr =>
-                        new AssignmentStatementSyntax(idToken.text, expr, idToken.location))));
+                    assign.And(expression).Map<StatementSyntax>(expr =>
+                        new AssignmentStatementSyntax(idToken.text, expr, idToken.location)));
 
             var procedureStatement =
                 id.Bind(idToken =>
-                    Maybe(lparen.And(arguments.Bind(args => rparen.Map(_ => args))))
-                    .Map<StatementSyntax>(args =>
+                    Maybe(lparen.And(arguments).Bind(args => rparen.Map(_ => args))).Map<StatementSyntax>(args =>
                         new ProcedureStatementSyntax(new CallExpressionSyntax(idToken.text, args, idToken.location))));
 
             var compoundStatement =
                 begin.And(
                     ZeroOrMore(
-                        ZeroOrMore(semi).And(statement)).Bind(stmts =>
-                            ZeroOrMore(semi).And(end.And(Constant<StatementSyntax>(new CompoundStatementSyntax(stmts))))));
+                        ZeroOrMore(semi)
+                        .And(statement))
+                    .Bind(stmts =>
+                        ZeroOrMore(semi)
+                        .And(end)
+                        .And(Constant<StatementSyntax>(new CompoundStatementSyntax(stmts)))));
 
             statement.Parse =
                 compoundStatement
@@ -178,25 +186,25 @@ namespace SharpPascal.Syntax
 
             var varDeclaration =
                 id.Bind(name =>
-                    colon.And(id.Bind(type =>
-                        semi.And(Constant(new VarDeclarationSyntax(name.text, type.text, name.location))))));
+                    colon.And(id).Bind(type =>
+                        semi.And(Constant(new VarDeclarationSyntax(name.text, type.text, name.location)))));
 
             var varSection =
-                var.And(varDeclaration.Bind(first =>
+                var.And(varDeclaration).Bind(first =>
                     ZeroOrMore(varDeclaration).Map(vars =>
                     {
                         vars.Insert(0, first);
                         return vars;
-                    })));
+                    }));
 
             var declarations =
                 varSection;
 
             var program =
                 Maybe(blank)
-                .And(Maybe(declarations).Bind(decls =>
+                .And(Maybe(declarations)).Bind(decls =>
                     compoundStatement.Bind(main =>
-                        dot.Map(_ => new UnitSyntax(main, decls)))));
+                        dot.Map(_ => new UnitSyntax(main, decls))));
 
             return program.ParseToCompletion(sourceText);
         }
